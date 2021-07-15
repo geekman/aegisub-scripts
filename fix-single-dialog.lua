d34e55d355c21a7d65dc20d43d8e304d480ce3db
Darell Tan <darell.tan@gmail.com>
2021-07-15 21:17:31 +0800

    Abstracted out utils into separate file
    This allows the utility functions to be shared across scripts, as the
    native string parsing functionality in Lua is really limiting.

--- fix-single-dialog.lua
+++ fix-single-dialog.lua
@@ -9,57 +9,7 @@ script_description = "Remove dash for single party dialog"
 script_author = "darell tan"
 script_version = "1"
 
--- Checks if pos comes after a newline.
--- It also skips over styling tags "{..}" o contine searching for newlines.
--- If a newline (or start of string) comes "immediately" before pos, return 
--- the index at which the newline was found, or 1 if it was the string start.
--- Otherwise, a 0 is returned, indicating it was not a newline preceding pos.
-function preceding_newline(t, pos)
-	if pos == 1 then return pos end
-
-	local intag = false
-	while pos > 1 do
-		pos = pos - 1
-		local c = t:sub(pos, pos)
-		--print(pos, c)
-
-		if c == '{' or c == '}' then
-			intag2 = c == '}'	-- next intag state
-			if intag == intag2 then		-- invalid state transition
-				break
-			else
-				intag = intag2
-			end
-
-			if not intag and pos == 1 then return pos end
-		elseif not intag then
-			if pos > 1 and t:sub(pos-1, pos) == "\\N" then
-				return pos
-			else
-				break
-			end
-		end
-	end
-	return 0
-end
-
--- Looks for an EOL following pos, if any.
--- If there is any whitespace at pos, it will be skipped. If no EOL immediately
--- follows, then nil is returned. Otherwise, the position of the newline is
--- returned.
-function next_eol(t, pos)
-	if pos > #t then return pos end
-
-	-- advance beyond whitespace, if any
-	local ws, ws_end = t:find('%s+', pos) 
-	if ws ~= nil and ws == pos then pos = ws_end end
-
-	if t:sub(pos, pos+1) == "\\N" then
-		return pos + 2
-	else
-		return nil
-	end
-end
+require 'gm-utils'
 
 function fix_single_dialog(subs, sel)
 	local pattern = "%s*[-]%s*"
